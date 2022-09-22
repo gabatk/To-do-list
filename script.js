1e2808d2c4c5223831bf7ca3d5aab37a56b94368
gabatk <gabriela.trybuszek@gmail.com>
2022-09-22 23:18:50 +0200

    Task validation form added.

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 const moreInfoCheck = document.querySelector('#gridCheck')
 const moreInfoForm = document.querySelector('.more-info')
+const taskContent = document.querySelector('#task-form')
+const taskCategory = document.querySelector('#inputState')
+const addBtn = document.querySelector('.add-task')
+// const formError = document.getElementById('#form-alert')
 
 
 const showMore = () => {
     moreInfoForm.classList.toggle('hide')
 }
 
+let text
 
+const validateForm = () => {
+    if (taskContent.value == '' && taskCategory.value == "Not defined"){
+    text = "Please type the task and check the category!"
+   } else if (taskContent.value !== '' && taskCategory.value == "Not defined"){
+    text = "Please check the category!"
+   } else if (taskContent.value == '' && taskCategory.value !== "Not defined"){
+    text = "Please type the task!"
+   } 
+//    formError.innerHTML = text
+   document.getElementById("form-alert").innerHTML = text
+        
+    }
 
 
 
@@ -15,3 +32,6 @@ const showMore = () => {
 
 
 moreInfoCheck.addEventListener('click',showMore)
+addBtn.addEventListener('click',validateForm)
+
+// TODO: sprawdzić konwencje nazewnictwa id

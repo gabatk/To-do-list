5f648c7c9ca53fa4d801bdee1784416283a8d40b
gabatk <gabriela.trybuszek@gmail.com>
2022-09-22 21:36:50 +0200

    Function to display more information implemented.

---
+++ script.js
@@ -0,0 +1,17 @@
+const moreInfoCheck = document.querySelector('#gridCheck')
+const moreInfoForm = document.querySelector('.more-info')
+
+
+const showMore = () => {
+    moreInfoForm.classList.toggle('hide')
+}
+
+
+
+
+
+
+
+
+
+moreInfoCheck.addEventListener('click',showMore)

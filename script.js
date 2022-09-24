334ac7239e7820d755b8f5a5d8ed5c6c8ffc4587
gabatk <gabriela.trybuszek@gmail.com>
2022-09-24 22:13:41 +0200

    Some CSS styles improved.

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ let text;
 const notDefinedValue = "Not defined";
 // document.getElementById('task-date') = new Date().toLocaleDateString()
 
-
 const validateForm = () => {
   text = "";
   if (taskContent.value == "" && taskCategory.value == notDefinedValue) {
@@ -34,10 +33,10 @@ const addTask = () => {
 
   if (text === "") {
     const liItem = document.createElement("li");
-    liItem.classList.add('liElement')
-    liItem.textContent = taskContent.value
-    createToolsArea(liItem)
-    ulList.append(liItem)
+    liItem.classList.add("liElement");
+    liItem.textContent = taskContent.value;
+    createToolsArea(liItem);
+    ulList.append(liItem);
   }
 
   taskContent.value = "";
@@ -54,25 +53,25 @@ const addTask = () => {
 // }
 
 const createToolsArea = (tools) => {
-  const toolsPanel = document.createElement('div')
-  toolsPanel.classList.add('tools')
-  tools.append(toolsPanel)
-  
-  const confirmIcon = document.createElement('button')
-  confirmIcon.classList.add('confirm')
-  confirmIcon.innerHTML = '<i class="fa-solid fa-check icon fa-lg fa-fw"></i>'
+  const toolsPanel = document.createElement("div");
+  toolsPanel.classList.add("tools");
+  tools.append(toolsPanel);
 
-  const editIcon = document.createElement('button')
-  editIcon.classList.add('edit')
-  editIcon.innerHTML = '<i class="fa-solid fa-pen-to-square icon fa-lg fa-fw"></i>'
+  const confirmIcon = document.createElement("button");
+  confirmIcon.classList.add("confirm");
+  confirmIcon.innerHTML = '<i class="fa-solid fa-check icon fa-lg fa-fw"></i>';
 
-  const deleteIcon = document.createElement('button')
-  deleteIcon.classList.add('delete')
-  deleteIcon.innerHTML = '<i class="fa-solid fa-xmark icon fa-lg fa-fw"></i>'
+  const editIcon = document.createElement("button");
+  editIcon.classList.add("edit");
+  editIcon.innerHTML =
+    '<i class="fa-solid fa-pen-to-square icon fa-lg fa-fw"></i>';
 
-  toolsPanel.append(confirmIcon, editIcon, deleteIcon)
- 
-}
+  const deleteIcon = document.createElement("button");
+  deleteIcon.classList.add("delete");
+  deleteIcon.innerHTML = '<i class="fa-solid fa-xmark icon fa-lg fa-fw"></i>';
+
+  toolsPanel.append(confirmIcon, editIcon, deleteIcon);
+};
 
 moreInfoCheck.addEventListener("click", showMore);
 addBtn.addEventListener("click", addTask);
